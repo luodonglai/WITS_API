a0d032267038acc08efa588ae459627801eff1c7
Donglai <luodonglai@gmail.com>
2017-12-22 10:03:14 +0100

    improve performance
    By incorporating more partner countries in one request.

--- wits_api.py
+++ wits_api.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
 
 # For the country data availability http://wits.worldbank.org/API/V1/wits/datasource/tradestats-trade/dataavailability/
 
-
+# For product code: http://wits.worldbank.org/API/V1/wits/datasource/tradestats-trade/product/all
 
 # get available indicators
 
@@ -53,6 +53,71 @@ indi = rq.urlopen('http://wits.worldbank.org/API/V1/wits/datasource/tradestats-t
 
 test = et.parse(indi)
 indi_list = et.tostring(test, pretty_print = True)
+
+# define single countr-partner-year-indicator-prodcut parser
+def trade_parser(json_url = "http://wits.worldbank.org/API/V1/SDMX/V21/datasource/tradestats-trade/reporter/usa/year/all/partner/chn/product/Total/indicator/MPRT-TRD-VL?format=JSON"):
+    response_totalimport = requests.get(json_url)
+    json_totalimport = response_totalimport.json()
+    
+    df_obtained = pd.DataFrame({})
+    # Loop for every partner country
+    for par in range(len(json_totalimport['dataSets'][0]['series'])):
+        # for each country:    
+        # retrieve the values of indicator for all the years
+        value_index = '0:0:' + str(par) + ':0:0'
+        
+        sv_obs = json_totalimport['dataSets'][0]['series'][value_index]['observations']
+
+        # shape it into a pandas dataframe
+        sl_obs = pd.DataFrame.from_dict(sv_obs,  orient = 'index') 
+        obs_value = sl_obs[0]
+        obs_value = obs_value.values
+        sl_obs.rename(columns = {sl_obs.columns[0] : 'value'}, inplace = True)
+
+
+        # retrieve all the available years
+        sv_year = json_totalimport['structure']['dimensions']['observation'][0]['values']
+        sl_year = pd.DataFrame.from_dict(sv_year, orient = 'columns')
+        sl_year.rename(columns = {sl_year.columns[1] : 'year'}, inplace = True)
+
+        # ------------------------------------------- #
+        # report the params of this request for the specific country
+        #freq =  annual
+        sv_freq = json_totalimport['structure']['dimensions']['series'][0]['values'][0]['id']
+
+        # Anchored the reporter, report its iso 3 code
+        sv_reporter = json_totalimport['structure']['dimensions']['series'][1]['values'][0]['id']
+
+        # Anchored the partner country, report its iso 3 code
+        sv_partner = json_totalimport['structure']['dimensions']['series'][2]['values'][par]['id']
+
+        # Anchored the product code, report its code which can be found in Trade: http://wits.worldbank.org/API/V1/wits/datasource/tradestats-trade/product/all
+        sv_prod = json_totalimport['structure']['dimensions']['series'][3]['values'][0]['id']
+
+        # Anchored the indicator, which can be found in Trade: http://wits.worldbank.org/API/V1/wits/datasource/tradestats-trade/indicator/ALL
+        sv_indi = json_totalimport['structure']['dimensions']['series'][4]['values'][0]['id']
+
+        # combine all the results
+        # obtain a good shape dataframe as container
+        df_combined = pd.DataFrame(pd.np.empty((sl_year.shape[0] , 7)), columns = {'year', 'value', 'freq', 'report', 'partner', 'prod', 'indi'})
+
+        df_combined['year'] = sl_year['year']
+        df_combined['report'] = sv_reporter
+        df_combined['partner'] = sv_partner
+        df_combined['prod'] = sv_prod
+        df_combined['indi'] = sv_indi
+        df_combined['freq'] = sv_freq
+
+        # iterate to fill in the value
+        for year in range(sl_year.shape[0]):
+            if str(year) in sv_obs.keys():
+                df_combined['value'][year] = sv_obs[str(year)][0]
+            else:
+                df_combined['value'][year] = 0
+
+        # combine all the results to get one reporter - all partner - all yr
+        df_obtained = df_obtained.append(df_combined, ignore_index = True)
+    return(df_obtained)
 
 
 def wits_trade(rpt = 'usa', ptn = 'chn', pcd = 'Total', idt = 'MPRT-TRD-VL', yr_def = 'all'): 
@@ -74,56 +139,11 @@ def wits_trade(rpt = 'usa', ptn = 'chn', pcd = 'Total', idt = 'MPRT-TRD-VL', yr_
 
     full_url = dashjoin.join( (preurl, rpurl ,rpt , yrurl, yr_def, pturl , ptn, pcurl, pcd, idurl, posturl) )
 
-    # response_totalimport = requests.get("http://wits.worldbank.org/API/V1/SDMX/V21/datasource/tradestats-trade/reporter/usa/year/all/partner/chn/product/Total/indicator/MPRT-TRD-VL?format=JSON")
-    response_totalimport = requests.get(full_url)
-    json_totalimport = response_totalimport.json()
-    #jnorm(json_totalimport)
+    # response_totalimport = requests.get("http://wits.worldbank.org/API/V1/SDMX/V21/datasource/tradestats-trade/reporter/usa/year/all/partner/all/product/Total/indicator/MPRT-TRD-VL?format=JSON")
     
-    # retrieve the values of indicator for all the years
-    sv_obs = json_totalimport['dataSets'][0]['series']['0:0:0:0:0']['observations']
+    df_target = trade_parser(full_url)
 
-    # shape it into a pandas dataframe
-    sl_obs = pd.DataFrame.from_dict(sv_obs,  orient = 'index') 
-    obs_value = sl_obs[0]
-    obs_value = obs_value.values
-    sl_obs.rename(columns = {sl_obs.columns[0] : 'value'}, inplace = True)
-
-
-    # retrieve all the available years
-    sv_year = json_totalimport['structure']['dimensions']['observation'][0]['values']
-    sl_year = pd.DataFrame.from_dict(sv_year, orient = 'columns')
-    sl_year.rename(columns = {sl_year.columns[1] : 'year'}, inplace = True)
-
-    # ------------------------------------------- #
-    # report the params of this request
-    #freq =  annual
-    sv_freq = json_totalimport['structure']['dimensions']['series'][0]['values'][0]['id']
-
-    # Anchored the reporter, report its iso 3 code
-    sv_reporter = json_totalimport['structure']['dimensions']['series'][1]['values'][0]['id']
-
-    # Anchored the partner country, report its iso 3 code
-    sv_partner = json_totalimport['structure']['dimensions']['series'][2]['values'][0]['id']
-
-    # Anchored the product code, report its code which can be found in Trade: http://wits.worldbank.org/API/V1/wits/datasource/tradestats-trade/product/all
-    sv_prod = json_totalimport['structure']['dimensions']['series'][3]['values'][0]['id']
-
-    # Anchored the indicator, which can be found in Trade: http://wits.worldbank.org/API/V1/wits/datasource/tradestats-trade/indicator/ALL
-    sv_indi = json_totalimport['structure']['dimensions']['series'][4]['values'][0]['id']
-
-    # combine all the results
-    # obtain a good shape dataframe as container
-    df_combined = pd.DataFrame(pd.np.empty((sl_year.shape[0] , 7)), columns = {'year', 'value', 'freq', 'report', 'partner', 'prod', 'indi'})
-
-    df_combined['year'] = sl_year['year']
-    df_combined['value'] = obs_value
-    df_combined['report'] = sv_reporter
-    df_combined['partner'] = sv_partner
-    df_combined['prod'] = sv_prod
-    df_combined['indi'] = sv_indi
-    df_combined['freq'] = sv_freq
-
-    return(df_combined)
+    return(df_target)
 
 
 # start the loop  for required data
